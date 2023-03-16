52a21ff7c3db8dddaa88dbd7469e43bc1a7a3e0c
Luciano655dev <lucianomenezes655@gmail.com>
2023-03-15 21:40:31 -0300

    frontend adicionado, muito no início

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config({ path: __dirname + "/.env" })
 const { twitterClient } = require("./twitterClient.js")
+const fs = require('fs')
+
+let tweetsObj = []
 
 setInterval(async()=>{
   try {
@@ -9,9 +12,14 @@ setInterval(async()=>{
     for(let i=0; i<280; i++){
         str.push(alfabeto[Math.round(Math.random() * (25 - 0) + 0)])
     }
-    await twitterClient.v2.tweet(str.join(''));
+
+
+    const { data: createdTweet } = await twitterClient.v2.tweet(str.join(''));
+
+    tweetsObj.push({ text: str.join(''), src: `https://twitter.com/luciano655dev/status/${createdTweet.id}` })
+    fs.writeFile('src/tweets.json', JSON.stringify(tweetsObj, null, 2), (err)=>console.log(err))
 
   } catch (e) {
     console.log(e)
   }
-}, 1800000); // 1 Tweet a cada 30 minutos (1800000ms = 30m)
+}, 1000); // 1 Tweet a cada 30 minutos (1800000ms = 30m)
